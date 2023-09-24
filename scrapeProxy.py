c51f8b4865fb4eeab1c143a6161ba744f1b27e5d
bruh <145855444+bruhb0y@users.noreply.github.com>
2023-09-24 16:52:40 +0300

    Update scrapeProxy.py

--- scrapeProxy.py
+++ scrapeProxy.py
@@ -2,7 +2,6 @@ import requests
 
 def scrapeProxy():
     links = []
-    proxys = []
     with open("proxy_sources.txt", "r") as file:
         for line in file.readlines():
             links.append(line.strip())
@@ -10,19 +9,19 @@ def scrapeProxy():
     lamount = len(links)
     print(f"[ ! ] Successfully loaded {lamount} links with HTTP/HTTP's proxy.")
 
-    for link in links:
+   with open(filepath, 'a') as file:
         amount = 0
         for link in links:
             response = requests.get(link)
-            if response.ok:
-                proxys.append(response.text)
-                lines = len(response.text.split("\n"))
-                print(f"[ + ] {lines} lines appeared in http_proxy.txt")
+            if not response.ok:
+                print(f"[ - ] {link} returned {response.status_code}. Please check if this link is correct.")
+            else:
+                file.write(response.text + "\n")
 
-    with open("http_proxy.txt", "a") as file:
-        for proxy in proxys:
-            file.write(proxy)
-    file.truncate()
+                lines = len(response.text.split('\n'))
+                amount += lines
+                print(f"[ + ] {lines} lines appeared in proxy.txt")
 
-    print(f"[ ! ] Finished with {amount} proxies from {lamount} in proxy.txt! hi chat")
+    print(f"[ ! ] Finished with {amount} proxies from {lamount} in proxy.txt!")
+
 scrapeProxy()
